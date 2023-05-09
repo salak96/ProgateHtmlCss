fcf6ed13cc64c4453350ace0d87d66264636312f
lambang <sasangkakurniawan.com>
2023-05-09 14:03:15 +0700

    modal

--- Assets/main.js
+++ Assets/main.js
@@ -87,7 +87,7 @@ function validateForm(){
         alert("Terima kasih sudah menghubungi kami");
  }
 }
-
+//accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -117,3 +117,27 @@ for (i = 0; i < acc.length; i++) {
      }
  });
 }
+
+//modal
+function showModal() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+  
+    // Get the image and insert it inside the modal
+    var img = document.getElementById("img");
+    var modalImg = document.getElementById("modalImg");
+    var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+  
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    console.log(showModal);
+  }
+  
